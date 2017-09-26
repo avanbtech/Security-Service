c8e79a39d28a9278b2d9bf18734dfcb20949ca00
Alex Zhang <cwzhang@sfu.ca>
2017-09-25 19:43:41 -0700

    Added everyone to the contact us page and added some styling.

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -7,8 +7,18 @@ function Contact({ title }) {
     <div className={s.root}>
       <div className={s.container}>
         <h1>{title}</h1>
-        <p>Raymond Huang xiruih@sfu.ca</p>
-        <p>Junru Tao junrut@sfu.ca</p>
+
+        {/*This part of the code can be encapsulated better in a loop. A lot of duplicate code.*/}
+        <p><b>Raymond Huang</b>: <i>xiruih@sfu.ca</i></p>
+        <p><b>Junru Tao</b>: <i>junrut@sfu.ca</i></p>
+        <p><b>Alex Zhang</b>: <i>cwzhang@sfu.ca</i></p>
+        <p><b>Angel Singh</b>: <i>asa179@sfu.ca</i></p>
+        <p><b>Lester Chee</b>: <i>lochee@sfu.ca</i></p>
+        <p><b>Sankait Kumar</b>: <i>sankaitk@sfu.ca</i></p>
+        <p><b>Jeff Yan</b>: <i>mingjiey@sfu.ca</i></p>
+        {/*Add Pharah's real full name here:*/}
+        <p><b>Pharah</b>: <i>fnobakht@sfu.ca</i></p>
+        <p><b>Contact Everyone</b>: <i>373-gamma@sfu.ca</i></p>
       </div>
     </div>
   );
