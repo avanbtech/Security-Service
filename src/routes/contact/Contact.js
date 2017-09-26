c30b73c87653982a81d3e64790dbe2a558f24875
Alex Zhang <cwzhang@sfu.ca>
2017-09-25 20:01:51 -0700

    Refactored name display and added comments of what I did and to be done.

--- src/routes/contact/Contact.js
+++ src/routes/contact/Contact.js
@@ -8,21 +8,30 @@ function Contact({ title }) {
       <div className={s.container}>
         <h1>{title}</h1>
 
-        {/*This part of the code can be encapsulated better in a loop. A lot of duplicate code.*/}
-        <p><b>Raymond Huang</b>: <i>xiruih@sfu.ca</i></p>
-        <p><b>Junru Tao</b>: <i>junrut@sfu.ca</i></p>
-        <p><b>Alex Zhang</b>: <i>cwzhang@sfu.ca</i></p>
-        <p><b>Angel Singh</b>: <i>asa179@sfu.ca</i></p>
-        <p><b>Lester Chee</b>: <i>lochee@sfu.ca</i></p>
-        <p><b>Sankait Kumar</b>: <i>sankaitk@sfu.ca</i></p>
-        <p><b>Jeff Yan</b>: <i>mingjiey@sfu.ca</i></p>
-        {/*Add Pharah's real full name here:*/}
-        <p><b>Pharah</b>: <i>fnobakht@sfu.ca</i></p>
-        <p><b>Contact Everyone</b>: <i>373-gamma@sfu.ca</i></p>
+{/*ContactInfo Function is below this function*/}
+    <ContactInfo/>
+
       </div>
     </div>
   );
 }
+
+{/*This part of the code can be encapsulated better by moving the function elsewhere. Storing our names and emails in a database would be even better to allow CSS styling of both email and names*/}
+var ContactInfo = React.createClass({
+    render: function() {
+
+          {/*Add Pharah's real full name here:*/}
+        var names = ['Raymond Huang: xiruih@sfu.ca', 'Junru Tao: junrut@sfu.ca', 'Alex Zhang: cwzhang@sfu.ca',
+        'Angel Singh: asa179@sfu.ca', 'Lester Chee: lochee@sfu.ca', 'Sankait Kumar: sankaitk@sfu.ca', 'Jeff Yan: mingjiey@sfu.ca',
+        'Pharah: fnobakht@sfu.ca', 'Contact Everyone: 373-gamma@sfu.ca'];
+
+        var namesList = names.map(function(name){
+                        return <li> <p><b>{name}</b></p> </li>;
+                      })
+
+        return  <ul>{ namesList }</ul>
+    }
+});
 
 Contact.propTypes = { title: PropTypes.string.isRequired };
 
