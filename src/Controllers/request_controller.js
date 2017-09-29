d5c9b65afd71394bc94028be91e38910517b684e
fnobakht@sfu.ca <frnknb@gmailcom>
2017-09-28 22:49:27 -0700

    Check input fields in body of main page

--- src/Controllers/request_controller.js
+++ src/Controllers/request_controller.js
@@ -2,6 +2,24 @@
 
 exports.request_post = function(req, res, next) {
   console.log(req.body);
-  
+
+  req.checkBody('date', 'date name must be specified').notEmpty();
+  req.checkBody('department', 'department must be specified').notEmpty();
+  req.checkBody('requestedBy', 'requestedBy name must be specified').notEmpty();
+  req.checkBody('phone', 'phone must be specified').notEmpty();
+  req.checkBody('fax', 'fax is optional');
+  req.checkBody('nameOfEvent', 'nameOfEvent name must be specified').notEmpty();
+  req.checkBody('licensed', 'licensed name must be specified').notEmpty();
+  req.checkBody('location', 'location name must be specified').notEmpty();
+  req.checkBody('eventDate', 'eventDate name must be specified').notEmpty();
+  req.checkBody('detail', 'detail name must be specified').notEmpty();
+  req.checkBody('firstName', 'firstName name must be specified').notEmpty();
+  req.checkBody('lastName', 'lastName name must be specified').notEmpty();
+  req.checkBody('userName', 'userName name must be specified').notEmpty();
+  req.checkBody('email', 'email name must be specified').notEmpty();
+  req.checkBody('id', 'id name must be specified').notEmpty();
+  req.checkBody('numberOfAttendees', 'numberOfAttendees name must be specified').notEmpty();
+  req.checkBody('time', 'time name must be specified').notEmpty();
+
   res.redirect('/');
 };
