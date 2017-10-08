9c76acf32df6f858ce7f37fe8beb4d942d451587
fnobakht@sfu.ca <frnknb@gmailcom>
2017-10-08 12:28:22 -0700

    Remove routes that are not used in application

--- src/routes.js
+++ src/routes.js
@@ -8,9 +8,6 @@ import ErrorPage from './components/ErrorPage';
 
 const routes = [
   require('./routes/home'),
-  require('./routes/contact'),
-  require('./routes/login'),
-  require('./routes/register'),
   require('./routes/customer'),
   require('./routes/status'),
   require('./routes/statusform'),
