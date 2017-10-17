0f6e3bedfaaf934042febf0cb7298364e792b983
fnobakht@sfu.ca <frnknb@gmailcom>
2017-10-16 22:23:48 -0700

    Show service requests using fake values

--- src/routes/service_view/index.js
+++ src/routes/service_view/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ServiceView from './ServiceView';
 import fetch from '../../core/fetch';
-const requestController = require('../../controllers/request_controller');
 
 export const path = '/ServiceView';
 export const action = async (state) => {
   const response = await fetch('/graphql?query={news{title,link,contentSnippet}}');
   const { data } = await response.json();
   state.context.onSetTitle('Service View');
-  return <ServiceView />;
+
+  var rows = [
+    {
+      requestBy: 'John',
+      date: '2017-10-12'
+    },
+    {
+      requestBy: 'Mary',
+      date: '2016-08-12'
+    }
+  ];
+  return <ServiceView serviceRequests={rows}/>;
 };
