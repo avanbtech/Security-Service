807f71b64ea2bbf182f0c38518ccf91ab01df15f
Sankait <sankaitk@sfu.ca>
2017-10-20 23:59:35 -0700

    Clean up

--- src/routes/service_view/index.js
+++ src/routes/service_view/index.js
@@ -1,32 +1,7 @@
 import React from 'react';
 import ServiceView from './ServiceView';
 import fetch from '../../core/fetch';
-import makePOST from './databaseQuery';
-
-
 import axios from 'axios';
-
-function getAllReqData() {
-  //console.log("\n\n\nHRE\n\n\n");
-  let res = null;
-
-  axios.post('http://localhost:3001/graphql', {
-    query: '{request{dbID}}',
-  })
-    .then(function (response) {
-      if(response['data']['data']['request']) {
-        res = response['data']['data']['request'];
-      }
-
-      return res;
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-  //return res;
-}
-
 
 export const path = '/ServiceView';
 export const action = async (state) => {
@@ -35,8 +10,6 @@ export const action = async (state) => {
   state.context.onSetTitle('Service View');
 
   // TODO: Replace following statement to retrieve data from database and store them in rows
-
-
   let res = null;
 
   await axios.post('http://localhost:3001/graphql', {
@@ -51,8 +24,6 @@ export const action = async (state) => {
       console.log(error.data);
     });
 
-  console.log(res);
-
   let rows = [];
 
   for(let x = 0; x < res.length; x++) {
@@ -65,36 +36,6 @@ export const action = async (state) => {
       event_date: res[x]['event']['eventDates'],
     });
   }
-
-  // for(var req in res) {
-  //   rows.push({
-  //     requestId: req['accessID'],
-  //     requestBy: req['user']['requestBy'],
-  //     date: req['date'],
-  //     sfu_id: "301298937",
-  //     location: "JOHN",
-  //     event_date: "DSFS",
-  //   });
-  // }
-
-  // var rows = [
-  //   {
-  //     requestId: '1',
-  //     requestBy: 'John',
-  //     date: '2017-10-12',
-  //     sfu_id: 'johnp',
-  //     location: 'Burnaby',
-  //     event_date: '2017-11-10'
-  //   },
-  //   {
-  //     requestId: '2',
-  //     requestBy: 'Mary',
-  //     date: '2016-08-12',
-  //     sfu_id: 'maryf',
-  //     location: 'Surrey',
-  //     event_date: '2017-12-08'
-  //   }
-  // ];
 
   return <ServiceView serviceRequests={rows}/>;
 };
