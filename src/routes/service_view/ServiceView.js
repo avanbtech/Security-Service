02bb37008d080a5f819b073da2b62f479910a487
fnobakht@sfu.ca <frnknb@gmailcom>
2017-10-24 22:12:36 -0700

    Add tbody to table Improve styling

--- src/routes/service_view/ServiceView.js
+++ src/routes/service_view/ServiceView.js
@@ -2,6 +2,11 @@ import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './ServiceView.scss';
 import OneServiceRequest from './OneServiceRequest';
+import FilterForm from './FilterForm';
+import {Form} from 'semantic-ui-react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import DatePicker from 'material-ui/DatePicker';
+
 
 function ServiceView({serviceRequests}) {
   var rows = [];
@@ -12,22 +17,24 @@ function ServiceView({serviceRequests}) {
   }
   return (
     <div className={s.root}>
-      <div className={s.filter_container}>
-        <FilterForm />
-      </div>
-      <div className={s.container}>
+        <div className={s.filter_container}>
+            <FilterForm />
+        </div>
+        <div className={s.container}>
         <h1 className={s.title}>Service View</h1>
         <table className={s.service_request_table}>
-          <tr>
-            <th>Request ID</th>
-            <th>Date</th>
-            <th>Status</th>
-            <th>Requested By</th>
-            <th>SFU ID</th>
-            <th>Location</th>
-            <th>Event Date</th>
-          </tr>
-          {rows}
+          <tbody>
+              <tr>
+                  <th>Request ID</th>
+                  <th>Date</th>
+                  <th>Status</th>
+                  <th>Requested By</th>
+                  <th>SFU ID</th>
+                  <th>Location</th>
+                  <th>Event Date</th>
+              </tr>
+              {rows}
+          </tbody>
         </table>
       </div>
     </div>
