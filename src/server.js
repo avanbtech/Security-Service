9cb82d4045747dcacf94afa78a458c42ded0666f
Alex <cwzhang@sfuca>
2017-10-26 22:16:02 -0700

    testing on windows, https seems working but exposes port

--- src/server.js
+++ src/server.js
@@ -15,8 +15,18 @@ import assets from './assets';
 import { port, auth, analytics } from './config';
 
 var expressValidator = require('express-validator');
-//var forceSSL = require('express-force-ssl');
-//var httpsRedirect = require('express-https-redirect');
+
+const HttpPort = 3000;
+const TLSPort = 3005;
+
+//function requireHTTPS(req, res, next) {
+  //  if (!req.secure) {
+    //    return res.redirect('https://' + req.get('host') + req.url);
+  //  }
+  //  next();
+//}
+
+
 
 const server = global.server = express();
 
@@ -35,26 +45,8 @@ server.use(cookieParser());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(expressValidator());
-//server.use(forceSSL);
-//server.set('forceSSLOptions', {
-//  enable301Redirects: true,
-//  trustXFPHeader: false,
-//  httpsPort: 3005,
-//  sslRequiredMessage: 'SSL Required.'
-//});
 
-//server.use('/', httpsRedirect());
 
-//
-// Authentication
-// -----------------------------------------------------------------------------
-// server.use(expressJwt({
-//   secret: auth.jwt.secret,
-//   credentialsRequired: false,
-//   /* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
-//   getToken: req => req.cookies.id_token,
-//   /* jscs:enable requireCamelCaseOrUpperCaseIdentifiers */
-// }));
 
 //
 // Register API middleware
@@ -121,48 +113,6 @@ server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   }));
 });
 
-
-// The following code launches the TLS v1.2 server
-// To be merged into main server when everything is suitable
-// Can be accessed at port: 3005
-// Created with reference to: https://www.sitepoint.com/how-to-use-ssltls-with-node-js/
-// https://nodejs.org/api/tls.html#tls_tls_ssl_concepts
-// https://github.com/nodejs/help/issues/253
-
-
-//Generate encrypted key in terminal with: openssl genrsa -des3 -out server.enc.key 1024
-
-
-//Issue certificate signing request with: openssl req -new -key server.enc.key -out server.csr
-//The following is our server.enc.key field entries:
-
-
-{/*Country Name (2 letter code) [AU]:CA
-State or Province Name (full name) [Some-State]:British Columbia
-Locality Name (eg, city) []:Vancouver
-Organization Name (eg, company) [Internet Widgits Pty Ltd]:Simon Fraser University
-Organizational Unit Name (eg, section) []:Security Services
-Common Name (e.g. server FQDN or YOUR name) []:CMPT 373
-Email Address []:cwzhang@sfu.ca
-
-Please enter the following 'extra' attributes
-to be sent with your certificate request
-A challenge password []:cmpt373
-An optional company name []:SFU */}
-
-
-//If required, remove the encryption/password with: openssl rsa -in server.enc.key -out server.key
-
-
-//Self sign the certificate with: openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-//Certificate lasts 365 days from creation.
-
-
-// To be completed. Need to make it work for server and client side handshakes, and have client generate a certificate as necessary.
-
-const HttpPort = 3004;
-const TLSPort = 3005;
-
 var fs = require('fs');
 var http = require('http');
 var https = require('https');
@@ -173,22 +123,10 @@ var options = {
     cert : fs.readFileSync('server.crt')
 };
 
-//http.createServer(server).listen(HttpPort, () => {
-//  console.log(`The http server is running at http://localhost:${port}/`);
-//});
-//Creates the HTTPS/TLS server on TLSPort
+http.createServer(server).listen(HttpPort, () => {
+  console.log(`The http server is running at http://localhost:${port}/`);
+});
+
 //https.createServer(options, server).listen(TLSPort, () => {
     // console.log('The HTTPS/TLS server is running on port ' + TLSPort);
 //});
-
-//
-// Launch the server
-// -----------------------------------------------------------------------------
-server.listen(port, () => {
-console.log(`The browsersync server is running at http://localhost:${port}/`);
-});
-
-//Test server to check end to end health
-const app = require('express')();
-app.get('/health-check', (req, res) => res.sendStatus(200));
-app.listen(8080);
