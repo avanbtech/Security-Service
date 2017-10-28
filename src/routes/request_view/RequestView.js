54cca998cc597adb57e0ea129feda4a3ab7608ea
fnobakht <fnobakht@sfu.ca>
2017-10-28 13:37:49 -0700

    Add a new route for handling requests

---
+++ src/routes/request_view/RequestView.js
@@ -0,0 +1,34 @@
+import React, { PropTypes } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './RequestView.scss';
+
+function ServiceView({request}) {
+  /*
+  var rows = [];
+  for(var i = 0; i < request.length; i++) {
+    rows.push(<OneServiceRequest serviceRequest={
+      request[i]
+    }/>);
+  }
+  */
+  return (
+    <div className={s.root}>
+      <div className={s.container}>
+        <h1 className={s.title}>Service View</h1>
+        <table className={s.service_request_table}>
+          <tr>
+            <th>Request ID</th>
+            <th>Date</th>
+            <th>Requested By</th>
+            <th>SFU ID</th>
+            <th>Location</th>
+            <th>Event Date</th>
+          </tr>
+          {rows}
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default withStyles(ServiceView, s);
---
+++ src/routes/request_view/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import RequestView from './RequestView';
+import fetch from '../../core/fetch';
+import dbMethods from '../../core/dbFetchMethods';
+
+export const path = '/ServiceView/:id';
+export const action = async (state) => {
+  const response = await fetch('/graphql?query={news{title,link,contentSnippet}}');
+  const { data } = await response.json();
+  state.context.onSetTitle('Request View');
+
+  console.lod(`Request ID: ` + state.conext.params.id);
+  let res = await dbMethods.getUserByReqID(state.conext.params.id);
+
+  let rows = [];
+
+  if(res !== null) {
+    for(let x = 0; x < res.length; x++) {
+      rows.push({
+        requestId: res[x]['accessID'],
+        requestBy: res[x]['user']['requestBy'],
+        date: res[x]['date'].split("T")[0],
+        sfu_id: res[x]['user']['sfuBCID'],
+        location: res[x]['event']['location'],
+        event_date: res[x]['event']['eventDates'],
+      });
+    }
+  }
+
+  return <RequestView request={rows}/>;
+};
