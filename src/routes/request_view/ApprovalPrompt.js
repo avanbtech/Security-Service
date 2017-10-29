fee07d22072973fdd2b68e18cd019a2cd4c48c90
fnobakht <fnobakht@sfu.ca>
2017-10-29 12:57:56 -0700

    Improving styling for approve/reject buttons

--- src/routes/request_view/ApprovalPrompt.js
+++ src/routes/request_view/ApprovalPrompt.js
@@ -1,25 +1,26 @@
 import React, { PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './RequestView.scss';
+import { Button,  Form, Message } from 'semantic-ui-react'
 
 function ApprovalPrompt(input) {
   return (
     <div>
         <div className={s.action_container}>
-          <form action={"/ServiceView/" + input.requestID} method="get">
+          <Form action={"/ServiceView/" + input.requestID} method="get">
             <input name='approved'
                    type='hidden'
                    value="yes" />
-            <input type='submit' value='Approve' />
-          </form>
+            <Form.Button type='submit'>Approve</Form.Button>
+          </Form>
         </div>
         <div className={s.action_container}>
-          <form action="/ServiceView/reject" method="post">
+          <Form action="/ServiceView/reject" method="post">
             <input name='requestID'
                    type='hidden'
                    value={input.requestID} />
-            <input type='submit' value='Reject' />
-          </form>
+            <Form.Button type='submit'>Reject</Form.Button>
+          </Form>
         </div>
     </div>
   );
