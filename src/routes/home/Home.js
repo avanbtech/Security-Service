f5a46e607164f8ff010ab0c1401d3c906da31148
xirui huang <xiruih@sfu.ca>
2017-10-28 23:39:09 -0700

    little design of main page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -4,13 +4,25 @@ import s from './Home.scss';
 import Link from '../../components/Link';
 import Main from '../../components/Main';
 
+
+import homebackground from '../../../test.jpg';
+import homebackground1 from '../../../test1.jpg';
+const homeImage = {
+    backgroundSize: '100% 100%', //记得这里100%
+    backgroundImage: 'url(' + homebackground + ')',
+    //或者下面这种也行
+    backgroundImage0: 'url(' + homebackground1 + ')'
+}
+
 function Home() {
   return (
-    <div className={s.root}>
-      <div className={s.container}>
-        <h1 className={s.title}></h1>
-        <Main className={s.main} />
-      </div>
+  	<div className="home" style={ homeImage }>
+	    <div className={s.root}>
+	      <div className={s.container}>
+	        <h1 className={s.title}></h1>
+	        <Main className={s.main} />
+	      </div>
+	    </div>
     </div>
   );
 }
