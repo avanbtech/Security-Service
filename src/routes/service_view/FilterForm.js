452ac69e7460a728af26e85524994aa68d45a3de
fnobakht <fnobakht@sfu.ca>
2017-10-30 20:48:10 -0700

    Change form from function to a class to be able to remember last selected date

--- src/routes/service_view/FilterForm.js
+++ src/routes/service_view/FilterForm.js
@@ -1,57 +1,100 @@
-import React, { PropTypes } from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './ServiceView.scss';
-import OneServiceRequest from './OneServiceRequest';
-import {Form} from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button,  Form, Message } from 'semantic-ui-react'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import DatePicker from 'material-ui/DatePicker';
+import s from './ServiceView.scss';
 
 
-function FilterForm({filterObject}) {
-  const start_date = filterObject.start_date === '' ? "From" :  filterObject.start_date;
-  const end_date = filterObject.end_date === '' ? "To" :  filterObject.end_date;
-
-  return (
-    <MuiThemeProvider>
-            <Form action="/ServiceView" method="GET">
-                <div className={s.filter}>
-                    <h4 className={s.filter_title}>Request Date</h4>
-                    <div className={s.filter_options}>
-                            <DatePicker
-                                hintText={start_date}
-                                container="inline"
-                                name="start_date"
-                                textFieldStyle={{
-                                    width: '100%',
-                                }}
-                            />
-                            <DatePicker
-                                hintText={end_date}
-                                container="inline"
-                                name="end_date"
-                                textFieldStyle={{
-                                    width: '100%',
-                                }}
-                            />
-                    </div>
-                </div>
-                <div className={s.filter}>
-                    <h4 className={s.filter_title}>Location</h4>
-                    <div className={s.filter_options}>
-                        <Form.Checkbox defaultChecked={filterObject.includeBurnaby}
-                          name = "include_burnaby" label='Burnaby' value="yes"/>
-                        <Form.Checkbox defaultChecked={filterObject.includeSurrey}
-                          name = "include_surrey" label='Surrey' value="yes"/>
-                        <Form.Checkbox defaultChecked={filterObject.includeVancouver}
-                          name = "include_vancouver" label='Vancouver' value="yes"/>
-                    </div>
-                </div>
-                <div className={s.button_container}>
-                    <Form.Button>Refine</Form.Button>
-                </div>
-            </Form>
-    </MuiThemeProvider>
-  );
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
 }
 
-export default withStyles(FilterForm, s);
+class FilterForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    let start_date = new Date();
+    if (props.filterObject.start_date !== '') {
+      start_date = addDays(new Date(props.filterObject.start_date), 1);
+    }
+    start_date.setFullYear(start_date.getFullYear());
+    start_date.setHours(0, 0, 0, 0);
+    let end_date = new Date();
+    if (props.filterObject.end_date !== '') {
+      end_date = addDays(new Date(props.filterObject.end_date), 1);
+    }
+
+    end_date.setFullYear(end_date.getFullYear());
+    end_date.setHours(0, 0, 0, 0);
+
+    this.state = {
+      start_date:start_date,
+      end_date:end_date
+    };
+  }
+
+  handleChangeStartDate = (event, date) => {
+    this.setState({
+      start_date: date,
+    });
+  }
+
+  handleChangeEndDate = (event, date) => {
+    this.setState({
+      end_date: date,
+    });
+  }
+
+  render(){
+    return (
+      <MuiThemeProvider>
+        <Form action="/ServiceView" method="GET">
+          <div className={s.filter}>
+            <h4 className={s.filter_title}>Request Date</h4>
+            <div className={s.filter_options}>
+              <DatePicker
+                hintText="From"
+                container="inline"
+                name="start_date"
+                defaultDate={this.state.start_date}
+                onChange={this.handleChangeStartDate}
+                textFieldStyle={{
+                  width: '100%',
+                }}
+              />
+              <DatePicker
+                hintText="To"
+                container="inline"
+                name="end_date"
+                defaultDate={this.state.end_date}
+                onChange={this.handleChangeEndDate}
+                textFieldStyle={{
+                  width: '100%',
+                }}
+              />
+            </div>
+          </div>
+          <div className={s.filter}>
+            <h4 className={s.filter_title}>Location</h4>
+            <div className={s.filter_options}>
+              <Form.Checkbox defaultChecked={this.props.filterObject.includeBurnaby}
+                             name = "include_burnaby" label='Burnaby' value="yes"/>
+              <Form.Checkbox defaultChecked={this.props.filterObject.includeSurrey}
+                             name = "include_surrey" label='Surrey' value="yes"/>
+              <Form.Checkbox defaultChecked={this.props.filterObject.includeVancouver}
+                             name = "include_vancouver" label='Vancouver' value="yes"/>
+            </div>
+          </div>
+          <div className={s.button_container}>
+            <Form.Button>Refine</Form.Button>
+          </div>
+        </Form>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default FilterForm
