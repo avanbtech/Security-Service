7bfe642fb3e5b95785d211e52896163c19128651
Jeff Yan <mingjiey@sfu.ca>
2017-10-30 19:16:59 -0700

    1. Fix the bug when the user press Enter to submit the input 2. Change the search button

--- src/components/CheckStatus/check.js
+++ src/components/CheckStatus/check.js
@@ -1,11 +1,8 @@
 import React, { Component } from 'react'
-import { Button,  Form, Message } from 'semantic-ui-react'
+import { Button,  Form, Icon } from 'semantic-ui-react'
 import TextField from "material-ui/TextField"
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
-import IconButton from 'material-ui/IconButton';
-import ActionHome from 'material-ui/svg-icons/action/find-in-page';
 
 class checkStatus extends Component {
 
@@ -54,24 +51,21 @@ class checkStatus extends Component {
         <MuiThemeProvider>
           <Form action="/status"
                 method="post">
+            <h2> Request Information </h2>
             <Form.Group widths='equal'>
-                <Form.Field required>
-                  <label> Check Status </label>
-                  <TextField
-                    name='referenceID'
-                    placeholder='Reference ID'
-                    onChange={this.handleChange}
-                    errorText={this.state.referenceIDError}
-                  />
-                </Form.Field>
+              <TextField
+                name='referenceID'
+                placeholder='Reference ID'
+                onChange={this.handleChange}
+                errorText={this.state.referenceIDError}
+              />
+              <Form.Button onClick = {e => this.onSubmit(e)} animated >
+                <Button.Content visible>Search</Button.Content>
+                <Button.Content hidden>
+                  <Icon name='search' />
+                </Button.Content>
+              </Form.Button>
             </Form.Group>
-            <IconButton 
-              tooltip="Search"
-              onClick = {e => this.onSubmit(e)}
-              href="/StatusForm">
-              Search
-              <ActionHome />
-            </IconButton>
           </Form>
         </MuiThemeProvider>
       )
