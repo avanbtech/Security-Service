156b1103d6d1f73f957764534a7bbe4bfc1ae7c7
asa179 <asa179@sfu.ca>
2017-11-03 01:46:48 -0700

    Redesign header
    -- Change colour theme to match SFU website
    -- Add SFU logo
    -- Remove unecessary images
    -- Adjust size of header
    -- Change headings
    -- Add side border

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,8 +3,9 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Header.scss';
 import Link from '../Link';
 import Navigation from '../Navigation';
-
-import logo from './logo1.png'
+import redbg from './textured-red-01-small.png'
+import logo from './sfu-logo@2x.png'
+import headerStrip from './header-strip.png'
 
 function Header() {
   return (
@@ -12,15 +13,16 @@ function Header() {
       <div className={s.container}>
         <Navigation className={s.nav} />
         <Link className={s.brand} to="/">
-          <div className = {s.container1}>
-
-           
-          </div>
-          <span className={s.brandTxt}>SFU</span>
+          <span className={s.brandTxt}>
+            <img src={logo} style={ {width: 300, height: 46 } }/>
+          </span>
         </Link>
         <div className={s.banner}>
           <h1 className={s.bannerTitle}>Security Request System</h1>
-          <p className={s.bannerDesc}>Simon Fraser University</p>
+        </div>
+        <div className={s.headerstrip}>
+        </div>
+        <div className={s.headerstripBG}>
         </div>
       </div>
     </div>
