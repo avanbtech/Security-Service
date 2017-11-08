e20336d10eb76125fbf85eebd3465b40a4c2aa47
asa179 <asa179@sfu.ca>
2017-11-07 20:28:31 -0800

    Make footer stay at bottom of page

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -3,6 +3,8 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Home.scss';
 import Link from '../../components/Link';
 import Main from '../../components/Main';
+import Footer from '../../components/Footer';
+import Feedback from '../../components/Feedback';
 
 
 import homebackground from '../../../test.jpg';
@@ -12,13 +14,18 @@ import homebackground from '../../../test.jpg';
 
 function Home() {
   return (
-
   	<div className="home">
 	    <div className={s.root}>
           <div className={s.blur}>
           </div>
         <div className={s.container}>
           <Main className={s.main} />
+        </div>
+        <div className={s.footer}>
+          <div className={s.footerContent}>
+            <Feedback />
+            <Footer />
+          </div>
         </div>
 	    </div>
     </div>
