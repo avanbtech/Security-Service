9240037ed85ae51b9caca3193f07a8fa37ee4974
asa179 <asa179@sfu.ca>
2017-11-22 01:21:44 -0800

    db password

--- src/core/db.js
+++ src/core/db.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 const Conn = new Sequelize(
   'demodb',
   'root',
-  'hooh392',
+  'dAnkmem3s',
   {
     dialect: 'mysql',
     host: 'localhost', //VM IP ADDRESS: '142.58.21.62',
