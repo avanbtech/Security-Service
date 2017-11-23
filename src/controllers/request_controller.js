c64c77c15c06e709623aa2b15700de20c5c5373d
Alex <cwzhang@sfuca>
2017-11-22 22:53:32 -0800

    Fully refactored request_controller.js

--- src/controllers/request_controller.js
+++ src/controllers/request_controller.js
@@ -7,6 +7,37 @@ import xoauth2 from 'xoauth2';
 
 import dbMethods from './dbCommitMethods';
 
+/*Helper Functions*/
+//Base function that sends different types of emails using EMAILSERVICE address
+//Takes req, subject line, and email contents as arguments.
+function sendMailTemplate(req, subjectLine, emailContents) {
+  let mailOptions = {
+    from: EMAILSERVICE,
+    to: req.body.email,
+    subject: subjectLine,
+    text: emailContents,
+  };
+  transporter.sendMail(mailOptions, (error, info)=>{
+     if(error){
+        console.log(error);
+    }
+    console.log("Email sent from sendMailTemplate successfully!");
+    console.log(info);
+   });
+}
+
+//Function called to escape and trim dbParam
+function escapeAndTrimInput(req, dbParam) {
+  req.filter(dbParam).escape();
+  req.filter(dbParam).trim();
+}
+
+//Checks if an input is empty in req, with a given dbParam Field, with a given emptyErrMessage.
+function checkIfInputIsEmptyInField(req, dbParam, emptyErrMessage) {
+  req.checkbody(dbParam, emptyErrMessage).notEmpty();
+}
+/*End Helper Functions definitions*/
+
 
 //Sets up the mailing server given parameters
 const EMAILSERVICE= 'gmail';
@@ -27,23 +58,6 @@ let transporter = nodemailer.createTransport({
   }
 });
 
-//Base function that sends different types of emails using EMAILSERVICE address
-//Takes req, subject line, and email contents as arguments.
-function sendMailTemplate(req, subjectLine, emailContents) {
-  let mailOptions = {
-    from: EMAILSERVICE,
-    to: req.body.email,
-    subject: subjectLine,
-    text: emailContents,
-  };
-  transporter.sendMail(mailOptions, (error, info)=>{
-     if(error){
-        console.log(error);
-    }
-    console.log("Email sent from sendMailTemplate successfully!");
-    console.log(info);
-   });
-}
 
 //Sends an email to the user
 function sendemailToUser(req) {
@@ -75,68 +89,61 @@ function sendemailToUserWithRejection(req) {
     console.log("sendemailToUserWithRejection function called successfully!");
 }
 
+//Checks request information input for whether or not they are empty
+function checkIfRequestInformationNotEmpty(req) {
 
+  checkIfInputIsEmptyInField(req, 'date', 'parameter: date must be specified');
+  checkIfInputIsEmptyInField(req, 'department', 'parameter: department must be specified');
+  checkIfInputIsEmptyInField(req, 'requestedBy', 'parameter: requestedBy must be specified');
+  checkIfInputIsEmptyInField(req, 'phone', 'parameter: phone must be specified');
+  checkIfInputIsEmptyInField(req, 'fax', 'parameter: fax is optional');
+  checkIfInputIsEmptyInField(req, 'nameOfEvent', 'parameter: nameOfEvent must be specified');
+  checkIfInputIsEmptyInField(req, 'licensed', 'parameter: licensed must be specified');
+  checkIfInputIsEmptyInField(req, 'location', 'parameter: location must be specified');
+  checkIfInputIsEmptyInField(req, 'eventDate', 'parameter: eventDate must be specified');
+  checkIfInputIsEmptyInField(req, 'detail', 'parameter: detail must be specified');
+  checkIfInputIsEmptyInField(req, 'firstName', 'parameter: firstName must be specified');
+  checkIfInputIsEmptyInField(req, 'lastName', 'parameter: lastName name must be specified');
+  checkIfInputIsEmptyInField(req, 'userName', 'parameter: userName name must be specified');
+  checkIfInputIsEmptyInField(req, 'email', 'parameter: email must be specified');
+  checkIfInputIsEmptyInField(req, 'id', 'parameter: id must be specified');
+  checkIfInputIsEmptyInField(req, 'numberOfAttendees', 'parameter: numberOfAttendees must be specified');
+  checkIfInputIsEmptyInField(req, 'time', 'parameter: time must be specified');
 
-function checkNotEmpty(req) {
-  req.checkBody('date', 'date name must be specified').notEmpty();
-  req.checkBody('department', 'department must be specified').notEmpty();
-  req.checkBody('requestedBy', 'requestedBy name must be specified').notEmpty();
-  req.checkBody('phone', 'phone must be specified').notEmpty();
-  req.checkBody('fax', 'fax is optional');
-  req.checkBody('nameOfEvent', 'nameOfEvent name must be specified').notEmpty();
-  req.checkBody('licensed', 'licensed name must be specified').notEmpty();
-  req.checkBody('location', 'location name must be specified').notEmpty();
-  req.checkBody('eventDate', 'eventDate name must be specified').notEmpty();
-  req.checkBody('detail', 'detail name must be specified').notEmpty();
-  req.checkBody('firstName', 'firstName name must be specified').notEmpty();
-  req.checkBody('lastName', 'lastName name must be specified').notEmpty();
-  req.checkBody('userName', 'userName name must be specified').notEmpty();
-  req.checkBody('email', 'email name must be specified').notEmpty();
-  req.checkBody('id', 'id name must be specified').notEmpty();
-  req.checkBody('numberOfAttendees', 'numberOfAttendees name must be specified').notEmpty();
-  req.checkBody('time', 'time name must be specified').notEmpty();
 }
 
-function filterInput(req) {
-  req.filter('date').escape();
-  req.filter('date').trim();
-  req.filter('department').escape();
-  req.filter('department').trim();
-  req.filter('requestedBy').escape();
-  req.filter('requestedBy').trim();
-  req.filter('phone').escape();
-  req.filter('phone').trim();
-  req.filter('fax').escape();
-  req.filter('fax').trim();
-  req.filter('nameOfEvent').escape();
-  req.filter('nameOfEvent').trim();
-  req.filter('licensed').escape();
-  req.filter('licensed').trim();
-  req.filter('location').escape();
-  req.filter('location').trim();
-  req.filter('eventDate').escape();
-  req.filter('eventDate').trim();
-  req.filter('detail').escape();
-  req.filter('detail').trim();
-  req.filter('firstName').escape();
-  req.filter('firstName').trim();
-  req.filter('lastName').escape();
-  req.filter('lastName').trim();
-  req.filter('userName').escape();
-  req.filter('userName').trim();
-  req.filter('email').escape();
-  req.filter('email').trim();
-  req.filter('id').escape();
-  req.filter('id').trim();
-  req.filter('numberOfAttendees').escape();
-  req.filter('numberOfAttendees').trim();
-  req.filter('time').escape();
-  req.filter('time').trim();
+//Escapes and filters each entry in the database
+function filterInputInRequestInformation(req) {
+
+var inputArray = [];
+
+inputArray.push('date');
+inputArray.push('department');
+inputArray.push('requestedBy');
+inputArray.push('phone');
+inputArray.push('fax');
+inputArray.push('nameOfEvent');
+inputArray.push('licensed');
+inputArray.push('location');
+inputArray.push('eventDate');
+inputArray.push('detail');
+inputArray.push('firstName');
+inputArray.push('lastName');
+inputArray.push('userName');
+inputArray.push('email');
+inputArray.push('id');
+inputArray.push('numberOfAttendees');
+inputArray.push('time');
+
+inputArray.forEach(function(element) {
+    escapeAndTrimInput(element);
+});
+
 }
 
 exports.request_post = function (req, res, next) {
-  checkNotEmpty(req);
-  filterInput(req);
+  checkIfRequestInformationNotEmpty(req);
+  filterInputInRequestInformation(req);
 
   dbMethods.commitRequestToDB(req);
   sendemailToUser(req);
@@ -144,64 +151,54 @@ exports.request_post = function (req, res, next) {
   res.redirect('/');
 };
 
+
 exports.get_accessID = function (req, res, next) {
-  req.checkBody('referenceID', 'Reference ID must be specified').notEmpty();
-  req.filter('referenceID').escape();
-  req.filter('referenceID').trim();
+  checkIfRequestInformationNotEmpty(req, 'referenceID', 'Reference ID must be specified');
+  escapeAndTrimInput(req, 'referenceID');
 
   res.redirect('/StatusForm/' + req.body.referenceID);
   console.log(req.body.referenceID);
 };
 
+//Sanitizes input in exports.request_approve
+function sanitizeRequestApprove(req) {
+  escapeAndTrimInput('requestID');
+
+  checkIfRequestInformationNotEmpty(req, 'supervisor', 'Supervisor must be specified');
+  checkIfRequestInformationNotEmpty(req, 'distribution', 'Distribution must be specified');
+  checkIfRequestInformationNotEmpty(req, 'guardRegularRate', 'Guard regular rate must be specified');
+  checkIfRequestInformationNotEmpty(req, 'guardRegularHours', 'Guard regular hours must be specified');
+  checkIfRequestInformationNotEmpty(req, 'guardOTRate', 'Guard overtime rate must be specified');
+  checkIfRequestInformationNotEmpty(req, 'guardOTHours', 'Guard overtime hours must be specified');
+  checkIfRequestInformationNotEmpty(req, 'scspRegularRate', 'SCSP regular rate must be specified');
+  checkIfRequestInformationNotEmpty(req, 'scspRegularHours', 'SCSP regular hours must be specified');
+  checkIfRequestInformationNotEmpty(req, 'scspOTRate', 'SCSP overtime rate must be specified');
+  checkIfRequestInformationNotEmpty(req, 'scspOTHours', 'SCSP overtime hours must be specified');
+  checkIfRequestInformationNotEmpty(req, 'totalGuardBillable', 'Total guard billable must be specified');
+  checkIfRequestInformationNotEmpty(req, 'totalSCSPBillable', 'Total SCSP billable must be specified');
+  checkIfRequestInformationNotEmpty(req, 'preparedBy', 'PreparedBy field must be specified');
+  checkIfRequestInformationNotEmpty(req, 'signature', 'No signature is provided');
+
+  escapeAndTrimInput(req, 'supervisor');
+  escapeAndTrimInput(req, 'distribution');
+  escapeAndTrimInput(req, 'distributionOther');
+  escapeAndTrimInput(req, 'guardRegularRate');
+  escapeAndTrimInput(req, 'guardRegularHours');
+  escapeAndTrimInput(req, 'guardOTRate');
+  escapeAndTrimInput(req, 'guardOTHours');
+  escapeAndTrimInput(req, 'scspRegularRate');
+  escapeAndTrimInput(req, 'scspRegularHours');
+  escapeAndTrimInput(req, 'scspOTRate');
+  escapeAndTrimInput(req, 'scspOTHours');
+  escapeAndTrimInput(req, 'totalGuardBillable');
+  escapeAndTrimInput(req, 'totalSCSPBillable');
+  escapeAndTrimInput(req, 'preparedBy');
+  escapeAndTrimInput(req, 'signature');
+}
+
 exports.request_approve = function (req, res, next) {
-  req.filter('requestID').escape();
-  req.filter('requestID').trim();
 
-  req.checkBody('supervisor', 'Supervisor must be specified').notEmpty();
-  req.checkBody('distribution', 'Distribution must be specified').notEmpty();
-  req.checkBody('guardRegularRate', 'Guard regular rate must be specified').notEmpty();
-  req.checkBody('guardRegularHours', 'Guard regular hours must be specified').notEmpty();
-  req.checkBody('guardOTRate', 'Guard overtime rate must be specified').notEmpty();
-  req.checkBody('guardOTHours', 'Guard overtime hours must be specified');
-  req.checkBody('scspRegularRate', 'SCSP regular rate must be specified').notEmpty();
-  req.checkBody('scspRegularHours', 'SCSP regular hours must be specified').notEmpty();
-  req.checkBody('scspOTRate', 'SCSP overtime rate must be specified').notEmpty();
-  req.checkBody('scspOTHours', 'SCSP overtime hours must be specified');
-  req.checkBody('totalGuardBillable', 'Total guard billable must be specified').notEmpty();
-  req.checkBody('totalSCSPBillable', 'Total SCSP billable must be specified').notEmpty();
-  req.checkBody('preparedBy', 'PreparedBy field must be specified').notEmpty();
-  req.checkBody('signature', 'No signature is provided').notEmpty();
-  req.filter('supervisor').escape();
-  req.filter('supervisor').trim();
-  req.filter('distribution').escape();
-  req.filter('distribution').trim();
-  req.filter('distributionOther').escape();
-  req.filter('distributionOther').trim();
-  req.filter('guardRegularRate').escape();
-  req.filter('guardRegularRate').trim();
-  req.filter('guardRegularHours').escape();
-  req.filter('guardRegularHours').trim();
-  req.filter('guardOTRate').escape();
-  req.filter('guardOTRate').trim();
-  req.filter('guardOTHours').escape();
-  req.filter('guardOTHours').trim();
-  req.filter('scspRegularRate').escape();
-  req.filter('scspRegularRate').trim();
-  req.filter('scspRegularHours').escape();
-  req.filter('scspRegularHours').trim();
-  req.filter('scspOTRate').escape();
-  req.filter('scspOTRate').trim();
-  req.filter('scspOTHours').escape();
-  req.filter('scspOTHours').trim();
-  req.filter('totalGuardBillable').escape();
-  req.filter('totalGuardBillable').trim();
-  req.filter('totalSCSPBillable').escape();
-  req.filter('totalSCSPBillable').trim();
-  req.filter('preparedBy').escape();
-  req.filter('preparedBy').trim();
-  req.filter('signature').escape();
-  req.filter('signature').trim();
-
+  sanitizeRequestApprove(req);
 
   dbMethods.commitApproveToDB(req);
   sendemailToUserWithApproval(req);
@@ -209,8 +206,7 @@ exports.request_approve = function (req, res, next) {
 };
 
 exports.request_reject = function (req, res, next) {
-  req.filter('requestID').escape();
-  req.filter('requestID').trim();
+  escapeAndTrimInput(req, 'requestID');
 
   sendemailToUserWithRejection(req);
 
@@ -223,9 +219,9 @@ exports.request_reject = function (req, res, next) {
 };
 
 exports.check_status = function (req, res, next) {
-  req.checkBody('referenceID', 'Reference ID must be specified').notEmpty();
-  req.filter('referenceID').escape();
-  req.filter('referenceID').trim();
+
+  checkIfRequestInformationNotEmpty(req, 'referenceID', 'Reference ID must be specified');
+  escapeAndTrimInput(req, 'requestID');
 
   res.redirect('/StatusForm');
 };
@@ -237,17 +233,21 @@ exports.export_to_pdf = function (req, res, next) {
   // Have to wait about 10 seconds to let the python script finish
   // running and then locate the file in the ExportedPDFs directory
   // Couldn't find a method to sleep thread in JS (wtf)
+
+  const waitTimeInMS = 10000;
+  const downloadPDFName = 'request.pdf';
+
   setTimeout(() => {
     let filePath = exportMethods.locateFilePath(req.body.referenceID, false);
 
     if (filePath) {
       console.log(`File path was: ${filePath}`);
 
-      res.download(filePath.toString(), 'request.pdf');
+      res.download(filePath.toString(), downloadPDFName);
     } else {
       console.log("INVALID PATH");
       // TODO: SHOW AN ALERT INSTEAD OF A REDIRECTION
       res.redirect("/");
     }
-  }, 10000);
+  }, waitTimeInMS);
 };
