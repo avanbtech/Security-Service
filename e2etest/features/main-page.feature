ce522ecb8725e7752d14959a23a8ec88ba27d214
junrut <junrut@sfu.ca>
2017-11-28 02:44:26 -0800

    Add step definitions in main-page-steps.js

--- e2etest/features/main-page.feature
+++ e2etest/features/main-page.feature
@@ -9,3 +9,25 @@ Feature: Main Page Test
     Then I should see the main page logo and background image
 
   Scenario: Browse the main page and get to the request form page
+    Given I am at the main page of the security request system
+    Then I should see the main page logo and background image
+    When I click the request form button
+    Then I should see the request form page
+
+  Scenario: Browse the main page and get to the status page
+    Given I am at the main page of the security request system
+    Then I should see the main page logo and background image
+    When I click the check status button
+    Then I should see the status page
+
+  Scenario: Browse the main page and get to the service view page
+    Given I am at the main page of the security request system
+    Then I should see the main page logo and background image
+    When I click the service view button
+    Then I should see the service view page
+
+  Scenario: Browse the main page and get to the export data page
+    Given I am at the main page of the security request system
+    Then I should see the main page logo and background image
+    When I click the export data button
+    Then I should see the export data page
--- e2etest/step-definitions/main-page-steps.js
+++ e2etest/step-definitions/main-page-steps.js
@@ -4,10 +4,46 @@ const mainPageObject = require('../page-objects/main-page-object');
 
 module.exports = function () {
   this.Given(/^I am at the main page of the security request system$/, function () {
-    return helpers.loadPage(mainPageObject.url);
+
+  	driver.manage().window().setSize(1280, 720);
+    return helpers.loadPage(mainPageObject.url).then(function () {
+    	driver.sleep(1800);
+    });
   });
 
   this.Then(/^I should see the main page logo and background image$/, function (callback) {
     callback();
+  });
+
+  this.When(/^I click the request form button$/, function (callback) {
+  	callback();
+  });
+
+  this.When(/^I click the check status button$/, function (callback) {
+  	callback();
+  });
+
+  this.When(/^I click the service view button$/, function (callback) {
+  	callback();
+  });
+
+  this.When(/^I click the export data button$/, function (callback) {
+  	callback();
+  });
+
+  this.Then(/^I should see the request form page$/, function (callback) {
+  	callback();
+  });
+
+  this.Then(/^I should see the status page$/, function (callback) {
+  	callback();
+  });
+
+  this.Then(/^I should see the service view page$/, function (callback) {
+  	callback();
+  });
+
+  this.Then(/^I should see the export data page$/, function (callback) {
+  	callback();
   });
 };
