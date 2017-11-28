766c44360d1864b76dfbd16665d2c0548387043b
fnobakht@sfu.ca <frnknb@gmailcom>
2017-11-27 20:44:16 -0800

    Adding a new path for receiving requests to convert guard info to PDF

---
+++ src/routes/guard_info_pdf/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import RequestView from './RequestView';
+import fetch from '../../core/fetch';
+import dbMethods from '../../data/dbFetchMethods';
+
+export const path = '/ServiceView/:id';
+export const action = async (state) => {
+  state.context.onSetTitle('Request View');
+  let approved = false;
+  if('approved' in state.query && state.query.approved == 'yes'){
+    approved = true;
+  }
+  let res = await dbMethods.getReqByID(state.params.id);
+  let rows = [];
+  return <RequestView
+            request={res}
+            requestID={state.params.id}
+            approved={approved}
+  />;
+};
