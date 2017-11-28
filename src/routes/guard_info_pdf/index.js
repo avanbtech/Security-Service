0aba904612b20f6813daea016f7100a4a8a87017
fnobakht@sfu.ca <frnknb@gmailcom>
2017-11-27 20:58:03 -0800

    Update guard info index to return a HTML tag (which will be replaced by a PDF document)

--- src/routes/guard_info_pdf/index.js
+++ src/routes/guard_info_pdf/index.js
@@ -4,5 +4,7 @@ export const path = '/ServiceView/export_guard_list/:id';
 export const action = async (state) => {
   //TODO: add code for converting guard info to PDF, use state.params.id to look up the guard
   console.log('Guard ID: ', state.params.id);
-  res.redirect('/');
+  return (
+    <p>Guard PDF Info</p>
+  );
 };
