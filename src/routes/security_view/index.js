c0f6a48ade9ce9c3cdcf427a760a74d789a0c976
asa179 <asa179@sfu.ca>
2017-11-28 18:20:52 -0800

    Add routing for security side page

--- src/routes/security_view/index.js
+++ src/routes/security_view/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SecurityViewHome from './SecurityViewHome';
 import fetch from '../../core/fetch';
 
-export const path = '/';
+export const path = '/Security';
 export const action = async (state) => {
   state.context.onSetTitle('SFU security request system');
   return <SecurityViewHome />;
