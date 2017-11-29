23aa17521e8ff3ef1952b44ffcaf0cad0ddc7b25
Alex <cwzhang@sfuca>
2017-11-29 02:03:55 -0800

    Refactored npm dependencies

--- tools/copy.js
+++ tools/copy.js
@@ -11,8 +11,8 @@ async function copy({ watch } = {}) {
   const ncp = Promise.promisify(require('ncp'));
 
   await Promise.all([
-    ncp('src/public', 'build/public'),
-    ncp('src/content', 'build/content'),
+    ncp('tools/npm-dependencies/public', 'build/public'),
+    ncp('tools/npm-dependencies/content', 'build/content'),
     ncp('package.json', 'build/package.json'),
   ]);
 
@@ -26,11 +26,11 @@ async function copy({ watch } = {}) {
 
   if (watch) {
     const watcher = await new Promise((resolve, reject) => {
-      gaze('src/content/**/*.*', (err, val) => err ? reject(err) : resolve(val));
+      gaze('tools/npm-dependencies/content/**/*.*', (err, val) => err ? reject(err) : resolve(val));
     });
     watcher.on('changed', async (file) => {
-      const relPath = file.substr(path.join(__dirname, '../src/content/').length);
-      await ncp(`src/content/${relPath}`, `build/content/${relPath}`);
+      const relPath = file.substr(path.join(__dirname, '../tools/npm-dependencies/content/').length);
+      await ncp(`tools/npm-dependencies/content/${relPath}`, `build/content/${relPath}`);
     });
   }
 }
